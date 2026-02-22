9a7ac76970012874d6bc755933d6be6ea94cad7c
Asteez <kukla1416@wp.pl>
2026-02-22 17:53:20 +0100

    Added sql scripts for faster manual testing

---
+++ backend/diet-ms/src/main/resources/data.sql
@@ -0,0 +1,13 @@
+INSERT INTO product_snapshot
+(product_id, name, product_category, calories, protein, carbs, fat, owner_username)
+VALUES
+(1, 'salmon', 'FISH', 670, 21, 0, 67, null),
+(2, 'turkey', 'MEAT', 121, 21, 0, 5, null),
+(3, 'banana', 'FRUIT', 100, 1, 0, 24, null),
+(4, 'orange', 'FRUIT', 100, 1, 0, 24, null),
+(5, 'strawberry', 'FRUIT', 100, 1, 0, 24, null),
+(6, 'raspberry', 'FRUIT', 100, 1, 0, 24, null),
+(7, 'dragon fruit', 'FRUIT', 100, 1, 0, 24, null),
+(8, 'chicken breast', 'MEAT', 121, 21, 0, 5, null),
+(9, 'beef burger', 'MEAT', 121, 21, 0, 5, 'user'),
+(10, 'chicken burger', 'MEAT', 121, 21, 0, 5, 'user');
