b6c8e7a64accf2d775f8aa68783cba3c898181a2
Asteez <kukla1416@wp.pl>
2026-04-27 18:41:23 +0200

    enabled configuration properties for event binding mapping

--- backend/mealtrackingapp-common/src/main/java/io/github/patrykkukula/mealtrackingapp_common/MealTrackingAppCommonApplication.java
+++ backend/mealtrackingapp-common/src/main/java/io/github/patrykkukula/mealtrackingapp_common/MealTrackingAppCommonApplication.java
@@ -1,9 +1,12 @@
 package io.github.patrykkukula.mealtrackingapp_common;
 
+import io.github.patrykkukula.mealtrackingapp_common.events.EventBindingConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(value = EventBindingConfig.class)
 public class MealTrackingAppCommonApplication {
 
 	public static void main(String[] args) {
