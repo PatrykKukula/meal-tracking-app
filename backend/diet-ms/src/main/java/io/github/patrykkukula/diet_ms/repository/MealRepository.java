838f80abcb35711d57f86f5f589c056de22e7195
Asteez <kukla1416@wp.pl>
2026-04-27 18:40:58 +0200

    added repository method

--- backend/diet-ms/src/main/java/io/github/patrykkukula/diet_ms/repository/MealRepository.java
+++ backend/diet-ms/src/main/java/io/github/patrykkukula/diet_ms/repository/MealRepository.java
@@ -2,8 +2,14 @@ package io.github.patrykkukula.diet_ms.repository;
 
 import io.github.patrykkukula.diet_ms.model.Meal;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface MealRepository extends JpaRepository<Meal, Long> {
+
+    @Query("SELECT m FROM Meal m JOIN FETCH m.dietDay WHERE m.mealId= :mealId")
+    public Optional<Meal> findByIdWithDietDay(Long mealId);
 }
