f56bc3a651f4faf71738c4df31b4d62a66939f7f
Asteez <kukla1416@wp.pl>
2026-04-27 18:31:27 +0200

    added method to evict DietDay Cache

--- backend/diet-ms/src/main/java/io/github/patrykkukula/diet_ms/cache/CacheUtils.java
+++ backend/diet-ms/src/main/java/io/github/patrykkukula/diet_ms/cache/CacheUtils.java
@@ -11,8 +11,9 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @RequiredArgsConstructor
 public class CacheUtils {
-    private CacheManager cacheManager;
+    private final CacheManager cacheManager;
     private static final String MONTHLY_DIETS = "monthlyDiets";
+    private static final String DIET_DAY = "dietDay";
 
     public void evictMonthlyDietsCache(DietDay dietDay) {
         String key = ("%s-%s-%s").formatted(
@@ -28,5 +29,21 @@ public class CacheUtils {
         else {
             log.info("[CACHE {} does not exists", MONTHLY_DIETS);
         }
+    }
+
+    public void evictDietDayCache(DietDay dietDay) {
+        String key = dietDay.getDietDayId() + "-" + dietDay.getOwnerUsername();
+        Cache cache = cacheManager.getCache(DIET_DAY);
+        if (cache != null) {
+            cache.evict(dietDay.getDietDayId());
+        }
+        else {
+            log.info("[CACHE {} does not exists", MONTHLY_DIETS);
+        }
+    }
+
+    public void evictCaches(DietDay dietDay) {
+        this.evictDietDayCache(dietDay);
+        this.evictMonthlyDietsCache(dietDay);
     }
 }
