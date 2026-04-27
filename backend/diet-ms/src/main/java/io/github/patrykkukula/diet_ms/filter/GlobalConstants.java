7361b4dbaed271286352b9dd570059ef1b8bc15e
Asteez <kukla1416@wp.pl>
2026-04-27 18:41:56 +0200

    removed irrelevant class

--- backend/diet-ms/src/main/java/io/github/patrykkukula/diet_ms/filter/GlobalConstants.java
+++
@@ -1,7 +0,0 @@
-package io.github.patrykkukula.diet_ms.filter;
-
-public class GlobalConstants {
-    private GlobalConstants(){}
-
-    public static final String CORRELATION_ID = "correlation-id";
-}
