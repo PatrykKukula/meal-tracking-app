fe576198ec8f6f1c2195f76af23e99e396ca9d86
Asteez <kukla1416@wp.pl>
2026-04-28 23:04:36 +0200

    changed primary key values in script for better testing

--- backend/product-ms/src/main/resources/data.sql
+++ backend/product-ms/src/main/resources/data.sql
@@ -1,13 +1,13 @@
 INSERT INTO product
 (product_id, name, product_category, calories, protein, carbs, fat, owner_username)
 VALUES
-(1, 'salmon', 'FISH', 670, 21, 0, 67, null),
-(2, 'turkey', 'MEAT', 121, 21, 0, 5, null),
-(3, 'banana', 'FRUIT', 100, 1, 0, 24, null),
-(4, 'orange', 'FRUIT', 100, 1, 0, 24, null),
-(5, 'strawberry', 'FRUIT', 100, 1, 0, 24, null),
-(6, 'raspberry', 'FRUIT', 100, 1, 0, 24, null),
-(7, 'dragon fruit', 'FRUIT', 100, 1, 0, 24, null),
-(8, 'chicken breast', 'MEAT', 121, 21, 0, 5, null),
-(9, 'beef burger', 'MEAT', 121, 21, 0, 5, 'user'),
-(10, 'chicken burger', 'MEAT', 121, 21, 0, 5, 'user');
+(999, 'salmon', 'FISH', 670, 21, 0, 67, null),
+(1000, 'turkey', 'MEAT', 121, 21, 0, 5, null),
+(1001, 'banana', 'FRUIT', 100, 1, 0, 24, null),
+(1002, 'orange', 'FRUIT', 100, 1, 0, 24, null),
+(1003, 'strawberry', 'FRUIT', 100, 1, 0, 24, null),
+(1004, 'raspberry', 'FRUIT', 100, 1, 0, 24, null),
+(1005, 'dragon fruit', 'FRUIT', 100, 1, 0, 24, null),
+(1006, 'chicken breast', 'MEAT', 121, 21, 0, 5, null),
+(1007, 'beef burger', 'MEAT', 121, 21, 0, 5, 'user'),
+(1007, 'chicken burger', 'MEAT', 121, 21, 0, 5, 'user');
