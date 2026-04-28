58fcf81ea814ef1f976f1c4e8c8dd5f30b77fdb9
Asteez <kukla1416@wp.pl>
2026-04-28 23:12:23 +0200

    enabled scheduling

--- backend/diet-ms/src/main/java/io/github/patrykkukula/diet_ms/DietMicroserviceApplication.java
+++ backend/diet-ms/src/main/java/io/github/patrykkukula/diet_ms/DietMicroserviceApplication.java
@@ -2,11 +2,13 @@ package io.github.patrykkukula.diet_ms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication(scanBasePackages = {"io.github.patrykkukula.mealtrackingapp_common",
 		"io.github.patrykkukula.diet_ms"})
 @EnableMethodSecurity
+@EnableScheduling
 public class DietMicroserviceApplication {
 
 	public static void main(String[] args) {
--- backend/product-ms/src/main/java/io/github/patrykkukula/product_ms/ProductMicroserviceApplication.java
+++ backend/product-ms/src/main/java/io/github/patrykkukula/product_ms/ProductMicroserviceApplication.java
@@ -2,12 +2,14 @@ package io.github.patrykkukula.product_ms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication(scanBasePackages = {"io.github.patrykkukula.mealtrackingapp_common",
         "io.github.patrykkukula.product_ms"}
 )
 @EnableMethodSecurity
+@EnableScheduling
 public class ProductMicroserviceApplication {
 
     public static void main(String[] args) {
