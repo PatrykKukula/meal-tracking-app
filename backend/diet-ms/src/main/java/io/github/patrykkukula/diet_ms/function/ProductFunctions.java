b7e224f0cdb867f45a12302dc8dd4fe2ee6eeb5e
Asteez <kukla1416@wp.pl>
2026-04-29 23:07:54 +0200

    improved log statements

--- backend/diet-ms/src/main/java/io/github/patrykkukula/diet_ms/function/ProductFunctions.java
+++ backend/diet-ms/src/main/java/io/github/patrykkukula/diet_ms/function/ProductFunctions.java
@@ -2,6 +2,7 @@ package io.github.patrykkukula.diet_ms.function;
 
 import io.github.patrykkukula.diet_ms.service.ProductSnapshotService;
 import io.github.patrykkukula.mealtrackingapp_common.events.product.ProductCreatedEvent;
+import io.github.patrykkukula.mealtrackingapp_common.events.product.ProductDeletedEvent;
 import io.github.patrykkukula.mealtrackingapp_common.events.product.ProductUpdatedEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,7 +20,7 @@ public class ProductFunctions {
     @Bean
     public Consumer<ProductCreatedEvent> productCreated() {
         return event -> {
-          log.info("ProductCreated Event received in diet_ms");
+          log.info("ProductCreated Event received in diet_ms for product ID: {}", event.productId());
           productSnapshotService.addProductSnapshot(event);
         };
     }
@@ -27,16 +28,16 @@ public class ProductFunctions {
     @Bean
     public Consumer<ProductUpdatedEvent> productUpdated() {
         return event -> {
-            log.info("ProductUpdated Event received in diet_ms");
+            log.info("ProductUpdated Event received in diet_ms for product ID: {}", event.productId());
             productSnapshotService.updateProductSnapshot(event);
         };
     }
 
     @Bean
-    public Consumer<Long> productDeleted() {
-        return productId -> {
-            log.info("ProductDeleted Event received in diet_ms");
-            productSnapshotService.deleteProductSnapshot(productId);
+    public Consumer<ProductDeletedEvent> productDeleted() {
+        return event -> {
+            log.info("ProductDeleted Event received in diet_ms for product ID: {}", event.productId());
+            productSnapshotService.deleteProductSnapshot(event.productId());
         };
     }
 }
