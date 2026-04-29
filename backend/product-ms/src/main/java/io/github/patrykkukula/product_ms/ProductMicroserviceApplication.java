8106a9b0d72a74199c34b1fd69d07b39334c5b3a
Asteez <kukla1416@wp.pl>
2026-04-29 23:08:55 +0200

    added PostConstruct to control Spring Cloud Stream bindings

--- backend/product-ms/src/main/java/io/github/patrykkukula/product_ms/ProductMicroserviceApplication.java
+++ backend/product-ms/src/main/java/io/github/patrykkukula/product_ms/ProductMicroserviceApplication.java
@@ -1,7 +1,13 @@
 package io.github.patrykkukula.product_ms;
 
+import io.github.patrykkukula.mealtrackingapp_common.events.EventBindingConfig;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
@@ -10,10 +16,19 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 )
 @EnableMethodSecurity
 @EnableScheduling
+@ConfigurationPropertiesScan(basePackages = "io.github.patrykkukula.mealtrackingapp_common")
+@Slf4j
+@RequiredArgsConstructor
 public class ProductMicroserviceApplication {
+    private final EventBindingConfig config;
 
     public static void main(String[] args) {
         SpringApplication.run(ProductMicroserviceApplication.class, args);
+    }
+
+    @PostConstruct
+    public void init() {
+        log.info("Bindings loaded: {}", config);
     }
 
 }
