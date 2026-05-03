4be3feecac6b23123bcae92a1420c54d753e5ca3
Asteez <kukla1416@wp.pl>
2026-05-03 14:06:53 +0200

    added async product functions

---
+++ backend/statistics-ms/src/main/java/io/github/patrykkukula/statistics_ms/function/ProductFunctions.java
@@ -0,0 +1,23 @@
+package io.github.patrykkukula.statistics_ms.function;
+
+import io.github.patrykkukula.mealtrackingapp_common.events.product.ProductAddedToMealEvent;
+import io.github.patrykkukula.statistics_ms.service.StatisticsService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.function.Consumer;
+
+@Configuration
+@Slf4j
+@RequiredArgsConstructor
+public class ProductFunctions {
+    private final StatisticsService statisticsService;
+
+    public Consumer<ProductAddedToMealEvent> productAddedToMealEvent() {
+        return event -> {
+            log.info("ProductAddedToMeal Event received in statistics_ms for product ID: {}", event.productId());
+            statisticsService.addProductToProductCount(event);
+        };
+    }
+}
