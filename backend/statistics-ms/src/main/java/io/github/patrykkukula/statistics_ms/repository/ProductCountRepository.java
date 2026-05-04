e4b5acab097882666d5f189a7d1c8f8973fba70c
Asteez <kukla1416@wp.pl>
2026-05-04 22:32:33 +0200

    changed wrong Pageable import

--- backend/statistics-ms/src/main/java/io/github/patrykkukula/statistics_ms/repository/ProductCountRepository.java
+++ backend/statistics-ms/src/main/java/io/github/patrykkukula/statistics_ms/repository/ProductCountRepository.java
@@ -1,12 +1,12 @@
 package io.github.patrykkukula.statistics_ms.repository;
 
 import io.github.patrykkukula.statistics_ms.model.ProductCount;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.awt.print.Pageable;
 import java.util.List;
 import java.util.Optional;
 
