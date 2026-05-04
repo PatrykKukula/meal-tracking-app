3c1b39ee76aec24df3d55276872d4794324e6b5f
Asteez <kukla1416@wp.pl>
2026-05-04 22:28:16 +0200

    updated repository

--- backend/statistics-ms/src/main/java/io/github/patrykkukula/statistics_ms/repository/ProductCountRepository.java
+++ backend/statistics-ms/src/main/java/io/github/patrykkukula/statistics_ms/repository/ProductCountRepository.java
@@ -2,8 +2,19 @@ package io.github.patrykkukula.statistics_ms.repository;
 
 import io.github.patrykkukula.statistics_ms.model.ProductCount;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.awt.print.Pageable;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ProductCountRepository extends JpaRepository<ProductCount, Long> {
+
+    public Optional<ProductCount> findByProductIdAndUsername(Long productId, String username);
+
+    @Query("SELECT pc FROM ProductCount pc WHERE pc.username =  :username")
+    public List<ProductCount> findMostUsedProductsByUsername(@Param(value = "username") String username, Pageable pageable);
 }
