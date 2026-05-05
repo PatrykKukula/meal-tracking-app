1319f7ad1f3ac39628467f4c140517a8d6bc836e
Asteez <kukla1416@wp.pl>
2026-05-05 22:36:14 +0200

    added more functions

--- backend/statistics-ms/src/main/java/io/github/patrykkukula/statistics_ms/function/ProductFunctions.java
+++ backend/statistics-ms/src/main/java/io/github/patrykkukula/statistics_ms/function/ProductFunctions.java
@@ -1,6 +1,8 @@
 package io.github.patrykkukula.statistics_ms.function;
 
 import io.github.patrykkukula.mealtrackingapp_common.events.product.ProductAddedToMealEvent;
+import io.github.patrykkukula.mealtrackingapp_common.events.product.ProductRemovedFromMealEvent;
+import io.github.patrykkukula.mealtrackingapp_common.events.product.ProductUpdatedInMealEvent;
 import io.github.patrykkukula.statistics_ms.dto.ProductCountDto;
 import io.github.patrykkukula.statistics_ms.service.StatisticsService;
 import lombok.RequiredArgsConstructor;
@@ -21,7 +23,31 @@ public class ProductFunctions {
 
             ProductCountDto productCountDto = statisticsService.addProductToProductCount(event);
 
-            log.info("ProductCount updated successfully: {}", productCountDto);
+
         };
+    }
+
+    public Consumer<ProductRemovedFromMealEvent> productRemovedFromMealEvent() {
+        return event -> {
+            log.info("ProductRemovedFromMeal Event received in statistics_ms for product ID: {}", event.productId());
+
+            ProductCountDto productCountDto = statisticsService.removeProductFromProductCount(event);
+
+            productCountUpdatedSuccessfullyLog(productCountDto);
+        };
+    }
+
+    public Consumer<ProductUpdatedInMealEvent> productUpdatedInMealEvent() {
+        return event -> {
+            log.info("ProductUpdatedInMeal Event received in statistics_ms for product ID: {}", event.productId());
+
+            ProductCountDto productCountDto = statisticsService.updateProductInProductCount(event);
+
+            productCountUpdatedSuccessfullyLog(productCountDto);
+        };
+    }
+
+    private void productCountUpdatedSuccessfullyLog(ProductCountDto productCountDto) {
+        log.info("ProductCount updated successfully: {}", productCountDto);
     }
 }
