45418ba11304050bbb58f131da670acbe58ed6b2
Asteez <kukla1416@wp.pl>
2026-05-21 20:17:27 +0200

    removed irrelevant @Transactional due to the propagation from root method

--- backend/diet-ms/src/main/java/io/github/patrykkukula/diet_ms/service/ProductSnapshotService.java
+++ backend/diet-ms/src/main/java/io/github/patrykkukula/diet_ms/service/ProductSnapshotService.java
@@ -1,41 +1,27 @@
 package io.github.patrykkukula.diet_ms.service;
 
-import io.github.patrykkukula.diet_ms.dto.ProductDtoRead;
 import io.github.patrykkukula.diet_ms.exception.ProductSnapshotNotFoundException;
 import io.github.patrykkukula.diet_ms.mapper.ProductSnapshotMapper;
-import io.github.patrykkukula.diet_ms.model.Meal;
 import io.github.patrykkukula.diet_ms.model.ProductSnapshot;
 import io.github.patrykkukula.diet_ms.repository.ProductSnapshotRepository;
 import io.github.patrykkukula.mealtrackingapp_common.events.product.ProductCreatedEvent;
 import io.github.patrykkukula.mealtrackingapp_common.events.product.ProductUpdatedEvent;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class ProductSnapshotService {
     private final ProductSnapshotRepository productSnapshotRepository;
-    
-    public List<ProductDtoRead> getProductsForMeal(Meal meal) {
-        return meal.getProductQuantities()
-                .stream()
-                .map(quantity -> {
-                    return ProductSnapshotMapper.mapProductSnapshotToProductDtoRead(quantity.getProductSnapshot(), quantity);
-                }).toList();
-    }
 
-    @Transactional
     public void addProductSnapshot(ProductCreatedEvent event) {
         ProductSnapshot productSnapshot = productSnapshotRepository.save(ProductSnapshot.fromEvent(event));
+
         log.info("ProductSnapshot created: {}", productSnapshot);
     }
 
-    @Transactional
     public void updateProductSnapshot(ProductUpdatedEvent event) {
         ProductSnapshot snapshot = findById(event.productId());
 
@@ -44,9 +30,9 @@ public class ProductSnapshotService {
         log.info("ProductSnapshot updated: {}", updatedSnapshot);
     }
 
-    @Transactional
     public void deleteProductSnapshot(Long productId) {
         productSnapshotRepository.deleteById(productId);
+
         log.info("ProductSnapshot deleted with ID: {}", productId);
     }
 
